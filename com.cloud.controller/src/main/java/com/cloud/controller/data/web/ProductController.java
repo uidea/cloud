81f7e17be621f140fbfb506686c3b579ec4736fd
liubin <aguai_liu@163.com>
2018-07-09 18:09:36 +0800

    swagger has been done

--- com.cloud.controller/src/main/java/com/cloud/controller/data/web/ProductController.java
+++ com.cloud.controller/src/main/java/com/cloud/controller/data/web/ProductController.java
@@ -8,6 +8,9 @@
 	 */
 package com.cloud.controller.data.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloud.model.core.User;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
 
 
 
@@ -36,9 +43,35 @@ import com.cloud.model.core.User;
 public class ProductController {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ApiOperation(value = "根据id获取产品信息", notes = "根据id获取产品信息", httpMethod = "GET", response = User.class)
 	public ResponseEntity<User> get(@PathVariable Long id) {
 		User user = new User();
 		
 		return ResponseEntity.ok(user);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ApiOperation(value = "添加一个新的产品")
+	@ApiResponses(value = { @ApiResponse(code = 405, message = "参数错误") })
+	public ResponseEntity<String> add(User product) {
+		return ResponseEntity.ok("SUCCESS");
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	@ApiOperation(value = "更新一个产品")
+	@ApiResponses(value = { @ApiResponse(code = 400, message = "参数错误") })
+	public ResponseEntity<String> update(User product) {
+		return ResponseEntity.ok("SUCCESS");
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	@ApiOperation(value = "获取所有产品信息", notes = "获取所有产品信息", httpMethod = "GET", response = User.class, responseContainer = "List")
+	public ResponseEntity<List<User>> getAllProducts() {
+		User user = new User();
+		user.setAddress("address");
+		user.setAge("12");
+		user.setName("lous");
+		user.setIdNo("123423523");
+		return ResponseEntity.ok(Arrays.asList(user, user));
 	}
 }
