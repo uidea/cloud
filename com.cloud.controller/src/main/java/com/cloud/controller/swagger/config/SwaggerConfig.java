4a320c612474fd8b1823a5568e46c12eb658f6dd
liubin <aguai_liu@163.com>
2018-09-07 10:35:15 +0800

    submit sql

--- com.cloud.controller/src/main/java/com/cloud/controller/swagger/config/SwaggerConfig.java
+++ com.cloud.controller/src/main/java/com/cloud/controller/swagger/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -31,13 +32,15 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("对外开放接口API 文档")
-				.description("HTTP对外开放接口")
-				.version("1.0.0").termsOfServiceUrl("http://xxx.xxx.com")
-//				.license("LICENSE")
-//				.licenseUrl("http://xxx.xxx.com")
-				.build();
+		 return new ApiInfoBuilder()
+		            .title("Swagger Petstore")
+		            .description("This is a sample server Petstore server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.")
+		            .license("Apache 2.0")
+		            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+		            .termsOfServiceUrl("")
+		            .version("1.0.0")
+		            .contact(new Contact("","", "apiteam@swagger.io"))
+		            .build();
 	}
 
 }
